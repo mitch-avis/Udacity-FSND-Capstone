d0f76549d6c18d5433efcbf567533fb19d4ecd00
mitch-avis <mitchavis@gmail.com>
2023-11-29 16:56:14 -0700

    Update basic route

--- capstone_app/routes/api.py
+++ capstone_app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 from capstone_app import log  # pylint: disable=R0401
 from capstone_app.config.env_config import Config
@@ -12,12 +12,10 @@ api = Blueprint("api", __name__)
 @api.route("/")
 def get_greeting():
     greeting = "Hello"
-    response = {}
-    response["greeting"] = greeting
     if EXCITED:
-        response["greeting"] = f"{greeting}!!!!! You are doing great in this Udacity project."
-    log.debug("greeting: %s", response["greeting"])
-    return jsonify(response)
+        greeting = f"{greeting}!!!!! You are doing great in this Udacity project."
+    log.debug("greeting: %s", greeting)
+    return greeting
 
 
 @api.route("/coolkids")
