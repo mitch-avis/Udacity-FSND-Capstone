c6d28e954d883be53dacbd3abc1cc2373a347930
mitch-avis <mitchavis@gmail.com>
2023-11-29 16:56:00 -0700

    Add db models

--- capstone_app/database/models.py
+++ capstone_app/database/models.py
@@ -1,5 +1,8 @@
+import json
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Date, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import DeclarativeBase
 
 from capstone_app.config.env_config import Config
 
@@ -10,11 +13,12 @@ DB_PORT = Config.DB_PORT
 DB_NAME = Config.DB_NAME
 
 db_path = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 db = SQLAlchemy()
 
 
 def setup_db(app, database_path=db_path):
-    """Binds a flask application and a SQLAlchemy service"""
+    """Binds a Flask application and a SQLAlchemy service"""
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
@@ -22,19 +26,114 @@ def setup_db(app, database_path=db_path):
     db.create_all()
 
 
-class Person(db.Model):
+class Base(DeclarativeBase):
     # pylint: disable=R0903
-    """Person"""
+    pass
 
-    __tablename__ = "People"
 
-    id = Column(db.Integer, primary_key=True)
-    name = Column(String)
-    catchphrase = Column(String)
+actor_in_movie = Table(
+    "actor_in_movie",
+    Base.metadata,
+    Column("actor_id", Integer, ForeignKey("actors.id"), primary_key=True),
+    Column("movie_id", Integer, ForeignKey("movies.id"), primary_key=True),
+)
 
-    def __init__(self, name, catchphrase=""):
+
+class Movie(db.Model):
+    """Movie Model"""
+
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(256), nullable=False)
+    release_date = Column(Date, nullable=False)
+    cast = db.relationship("Actor", secondary=actor_in_movie, backref="movies", lazy=True)
+
+    def __init__(self, title, release_date):
+        """Movie Constructor"""
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        """Inserts a new movie into the database"""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """Deletes a movie from the database"""
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        """Updates a movie in the database"""
+        db.session.commit()
+
+    def short(self):
+        """Short form representation of the movie model"""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "recipe": self.release_date,
+        }
+
+    def long(self):
+        """Long form representation of the movie model"""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "recipe": self.release_date,
+            "cast": [actor.name for actor in self.cast],
+        }
+
+    def __repr__(self):
+        return json.dumps(self.short())
+
+
+class Actor(db.Model):
+    """Actor Model"""
+
+    __tablename__ = "actors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(128), nullable=False)
+
+    def __init__(self, name, age, gender):
+        """Actor Constructor"""
         self.name = name
-        self.catchphrase = catchphrase
+        self.age = age
+        self.gender = gender
 
-    def format(self):
-        return {"id": self.id, "name": self.name, "catchphrase": self.catchphrase}
+    def insert(self):
+        """Inserts a new actor into the database"""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """Deletes a actor from the database"""
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        """Updates a actor in the database"""
+        db.session.commit()
+
+    def short(self):
+        """Short form representation of the actor model"""
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+    def long(self):
+        """Long form representation of the actor model"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+        }
+
+    def __repr__(self):
+        return json.dumps(self.short())
