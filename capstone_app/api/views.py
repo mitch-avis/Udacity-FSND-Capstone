e6540b14aacc5c7e897ff272bec540fd24d065a6
mitch-avis <mitchavis@gmail.com>
2023-11-29 18:21:15 -0700

    Rename `routes` to `api`

--- capstone_app/api/views.py
+++ capstone_app/api/views.py
@@ -1,16 +1,18 @@
 from flask import Blueprint
 
 from capstone_app import log  # pylint: disable=R0401
+from capstone_app.auth.decorators import requires_auth
 from capstone_app.config.env_config import Config
 
 EXCITED = Config.EXCITED
 
 
-api = Blueprint("api", __name__)
+api_bp = Blueprint("api", __name__, template_folder="templates")
 
 
-@api.route("/")
-def get_greeting():
+@api_bp.route("/")
+def home():
+    """Home endpoint."""
     greeting = "Hello"
     if EXCITED:
         greeting = f"{greeting}!!!!! You are doing great in this Udacity project."
@@ -18,6 +20,14 @@ def get_greeting():
     return greeting
 
 
-@api.route("/coolkids")
+@api_bp.route("/coolkids")
 def be_cool():
+    """Some other endpoint."""
     return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+
+@api_bp.route("/admin")
+@requires_auth
+def admin():
+    """Protected endpoint."""
+    return "test"
