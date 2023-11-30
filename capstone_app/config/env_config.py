a246503d532578dfc2db60471492f09e9066a02b
mitch-avis <mitchavis@gmail.com>
2023-11-30 01:45:27 -0700

    Update env vars

--- capstone_app/config/env_config.py
+++ capstone_app/config/env_config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 class Config:
     # pylint: disable=R0903
-    ALGORITHMS = ["RS256"]
+    ALGORITHMS = getenv("ALGORITHMS", "RS256")
     ALLOWED_ORIGINS = getenv("ALLOWED_ORIGINS", "*")
     API_AUDIENCE = getenv("DB_NAME", "capstone")
     AUTH0_DOMAIN = getenv("AUTH0_DOMAIN", "dev-5txjjo1g1jpy427b.us.auth0.com")
@@ -12,7 +12,6 @@ class Config:
     DB_PASSWORD = getenv("DB_PASSWORD", "")
     DB_PORT = getenv("DB_PORT", "5432")
     DB_USER = getenv("DB_USER", "postgres")
-    EXCITED = getenv("EXCITED", "False").lower() in ("true", "t", "1")
     FLASK_APP = getenv("FLASK_APP", "udacity_fsnd_capstone")
     FLASK_DEBUG = getenv("FLASK_DEBUG", "False").lower() in ("true", "t", "1")
     HOST = getenv("HOST", "0.0.0.0")
