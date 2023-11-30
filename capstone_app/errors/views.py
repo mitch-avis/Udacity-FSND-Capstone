7dc55fd495d4847b8bb6b2c3a4598a1d5c8ed9c1
mitch-avis <mitchavis@gmail.com>
2023-11-30 00:02:03 -0700

    Update error handlers

--- capstone_app/errors/views.py
+++ capstone_app/errors/views.py
@@ -6,46 +6,37 @@ from capstone_app import log  # pylint: disable=R0401
 errors_bp = Blueprint("errors", __name__)
 
 
-class APIException(Exception):
-    error = "Internal Server Error"
-    status_code = 500
+class APIError(Exception):
+    """APIError Exception"""
+
+    name = "Internal Server Error"
+    code = 500
 
     def __init__(self, error=None, value=str(), message=str()):
         super().__init__()
         if error is not None:
-            self.error = error[0]
-            self.status_code = error[1]
-        self.value = value
-        self.message = message
-
-    def to_dict(self):
-        response = {
-            "error": self.error,
-            "status_code": self.status_code,
-            "detail": {
-                "error": self.error,
-                "value": self.value,
-                "message": self.message,
-            },
+            self.name = error[0]
+            self.code = error[1]
+        self.description = {
+            "value": value,
+            "message": message,
         }
-        return response
 
 
-@errors_bp.app_errorhandler(APIException)
-def invalid_api_usage(error: APIException):
-    log.debug(str(error.to_dict()))
-    return jsonify(error.to_dict()), error.status_code
+def to_dict(error):
+    response = {
+        "code": error.code,
+        "error": error.name,
+        "message": error.description,
+    }
+    return response
 
 
 @errors_bp.app_errorhandler(Exception)
 def handle_exception(error: Exception):
-    if isinstance(error, HTTPException):
+    if isinstance(error, (APIError, HTTPException)):
         status_code = error.code
-        response = {
-            "error": error.name,
-            "status_code": status_code,
-            "detail": error.description,
-        }
+        response = to_dict(error)
     else:
         status_code = 500
         response = {
